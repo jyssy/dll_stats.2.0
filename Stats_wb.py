265aa8a04310167c1ab0e91cea1a20359b60046b
Jesse L <30397506+jyssy@users.noreply.github.com>
2020-06-30 18:38:48 -0500

    20200630.2

--- Stats_wb.py
+++ Stats_wb.py
@@ -12,19 +12,19 @@ ws3 = wb.create_sheet('CallNos', 2)
 
 items = pd.read_csv('stats_out.csv', header=None) # the actual item-count statistics for each cataloger
 items = dataframe_to_rows(items)
-for r_idx, row in enumerate(items, 0):
-    for c_idx, value in enumerate(row, 0):
+for r_idx, row in enumerate(items, 1):
+    for c_idx, value in enumerate(row, 1):
          ws1.cell(row=r_idx, column=c_idx, value=value)
 eBibedits = pd.read_csv('969_out.csv', header=None) # to count edits made to bibs for electronic, etc, not counted by item edits
 eBibedits = dataframe_to_rows(eBibedits)
-for r_idx, row in enumerate(eBibedits, 0):
-    for c_idx, value in enumerate(row, 0):
+for r_idx, row in enumerate(eBibedits, 1):
+    for c_idx, value in enumerate(row, 1):
          ws2.cell(row=r_idx, column=c_idx, value=value)
 callNos = pd.read_csv('CallNo_Count.csv', header=None) # to list the accumulation of physical materials in call# ranges
-callNos.sort_values(by=[0], axis=0, ascending=True, index=False, inplace=True)
+callNos.sort_values(by=[0], axis=0, ascending=True, inplace=True)
 callNos = dataframe_to_rows(callNos)
-for r_idx, row in enumerate(callNos, 0):
-    for c_idx, value in enumerate(row, 0):
+for r_idx, row in enumerate(callNos, 1):
+    for c_idx, value in enumerate(row, 1):
          ws3.cell(row=r_idx, column=c_idx, value=value)
 
 syear = input('What year is it? ')
