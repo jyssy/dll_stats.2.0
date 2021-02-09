6c6d6cc948246ba58ffbffd266983b1373a663b1
Jesse L <30397506+jyssy@users.noreply.github.com>
2021-02-09 13:54:48 -0600

    Update dll_catstats.py

--- dll_catstats.py
+++ dll_catstats.py
@@ -1,5 +1,5 @@
 # This is the master stats script that opens and closes the others in their proper order
-# Running the statistics script: 'dll_metastats.py, grabs and organizes 969 data, creates the necxessary workbook to pull these codes and Call#s together'
+# Running the statistics script: 'dll_metastats.py, grabs and organizes, physical item stats, original and copy stats (print and slectronic), 969 data, orderd call#s, and creates the necessary workbook to pull these codes and Call#s together'
 # Statistics for Cataloging/Metadata Department (run monthly)
 # Uses data from an OLE Report (item records and bib data)
 # Jesse A Lambertson lambertson@uchicago.edu
@@ -9,6 +9,8 @@ import sys, os
 exec(open('call_counts.py').read())
 exec(open('item_stats.py').read())
 exec(open('969_stats.py').read())
+# exec(open('print.py').read())
+# exec(open('electronic.py').read())
 exec(open('Stats_wb.py').read())
 
 print('Workbook for the month created')
