054b81182bc03538afda85e939e92848f12606b3
jyssy <jessel@phantomlandscapes.com>
2021-04-08 14:17:04 -0500

    updating import statements

--- electronic.py
+++ electronic.py
@@ -1,19 +1,20 @@
-#  Electronic general statistics using codes
-import sys, os, csv, operator
+# Electronic general statistics using codes
+import csv
+import operator
 
-#  Change the name of the actual csv to: 'Cataloging_stats.csv'
+# Change the name of the actual csv to: 'Cataloging_stats.csv'
 file_name = "969_in.csv"
 f = open(file_name, encoding='utf-8')
 csv_file = csv.reader(f)
 next(csv_file)
-eighth_column = [] #  empty list to store eighth column values
+eighth_column = [] # empty list to store eighth column values
 for line in csv_file:
     eighth_column.append(line[7])
-#  removes empty lines, puts in required header file, strips out old header file
+# removes empty lines, puts in required header file, strips out old header file
 dataList=[s for s in eighth_column if s.strip()]
-#  Replace 'Item note' with iniCOD
+# Replace 'Item note' with iniCOD
 dataList.insert(0, 'iniCOD')
-#  make the list object into a set object
+# make the list object into a set object
 dataSet=set(dataList)
 
 team = dict(jal='Jesse Lambertson ', psm='Pat Sayre-McCoy ', mcd='Melanie Dial ', den='Daryl Nelson ', jrs='Julie Stauffer ', naa='Nissy ', dmd='Dora Davis ', ini='Initial_header ')
@@ -30,20 +31,20 @@ codes = dict(PMO=' Print Monograph Original ', PML=' Print Monograph LC Copy ', 
                  DVO=' DVD Original ', DVC=' DVD OCLC Copy ', DVL=' DVD LC Copy ',
                  ADM=' Added-Volume Monograph ', ADS=' Added-Volume Serial')
 
-#  Loop through the set.
-#  Count number of occurrences of each set element in the list
-#  Includes PCC BibCO (lambertson). NACO is, by necessity, collected separately.
+# Loop through the set.
+# Count number of occurrences of each set element in the list
+# Includes PCC BibCO (lambertson). NACO is, by necessity, collected separately.
 
 with open('electerim.csv', 'w', newline='') as unsorted:
-    #  writer = csv.writer(stats_output, delimiter=':')
+    # writer = csv.writer(stats_output, delimiter=':')
     writer = csv.writer(unsorted, delimiter=':')
 
     for stat in dataSet:
             name_stat = team[stat[:3]]
             code_stat = codes[stat[-3:]]
             statCount =dataList.count(stat)
-#        """Using 'write' in the csv module to output the necessary
-#            info. """
+#        '''Using 'write' in the csv module to output the necessary
+#            info. '''
             if stat != 'iniCOD':
                 writer.writerow([name_stat, code_stat, str(statCount)])
             else:
@@ -51,7 +52,7 @@ with open('electerim.csv', 'w', newline='') as unsorted:
 
 interim_file = open('electerim.csv', 'r')
 data = csv.reader(interim_file, delimiter = ',')
-sortedlist = sorted(data, key=operator.itemgetter(0))    #  0 specifies according to first column we want to sort
+sortedlist = sorted(data, key=operator.itemgetter(0))    # 0 specifies according to first column we want to sort
 
 with open('electronic_out.csv', 'w', newline='') as final:
     fileWriter = csv.writer(final, delimiter=',')
